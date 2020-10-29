3cad7a522bc2a73cb395895353efe34f7ce7d819
soda <wangsuda@icloud.com>
2020-10-29 16:35:48 +0800

    feat: 更新打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,33 @@
-import typescript from 'rollup-plugin-typescript'
+// import json from '@rollup/plugin-json';
+import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 
 const input = 'src/index.ts';
 const output = {
-    dir: 'dist/',
-    name: '[name].[ext]',
-    format: 'esm'
+  dir: 'dist/',
+  name: '[name].[ext]',
+  format: 'esm',
 };
+
+const extensions = ['.ts', '.tsx'];
 const plugins = [
-    typescript(),
-    resolve(),
-    commonjs(),
+//   json(),
+  resolve(),
+  commonjs(),
+  typescript({
+    // babelHelpers: 'runtime',
+  }),
+  babel({
+    extensions,
+    exclude: 'node_modules/**',
+    babelHelpers: 'runtime',
+  }),
 ];
 
 export default {
-    input,
-    output,
-    plugins,
-}
+  input,
+  output,
+  plugins,
+};
